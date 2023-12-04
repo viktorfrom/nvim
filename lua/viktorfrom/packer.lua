38bcc47623f5781ae4d99a49c1c695b3cb96b967
viktorfrom <viktor.from91@gmail.com>
2023-12-04 15:02:42 +0100

    Add temp fix for file search

--- lua/viktorfrom/packer.lua
+++ lua/viktorfrom/packer.lua
@@ -8,8 +8,9 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  -- 'nvim-telescope/telescope.nvim', tag = '0.1.2',
 	  -- or                            , branch = '0.1.x',
+	  'nvim-telescope/telescope.nvim', branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
